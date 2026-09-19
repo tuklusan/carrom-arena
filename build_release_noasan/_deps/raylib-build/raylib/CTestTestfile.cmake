dacb8ffbb27b44401cc7b183ebad7b55c3a51562
tuklusan <tuklusan@sanyalnet.lan>
2026-09-19 15:31:54 +0000

    chore: untrack build artifacts

--- build_release_noasan/_deps/raylib-build/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sanyalnet/SOFTWARE-DEVELOPMENT/carrom/build_release_noasan/_deps/raylib-src/src
-# Build directory: /home/sanyalnet/SOFTWARE-DEVELOPMENT/carrom/build_release_noasan/_deps/raylib-build/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("external/glfw")
--- build_release_noasan/tests/CMakeFiles/test_trace_circular.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for test_trace_circular.
-# This may be replaced when dependencies are built.
